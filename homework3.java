8cd6e7cbeba5423cd1188c8f8c04d35ee073d334
sunnanye <89904422+sunnanye@users.noreply.github.com>
2021-09-07 20:30:41 +0800

    Add files via upload

---
+++ homework3.java
@@ -0,0 +1,18 @@
+package com.runoob.text;
+
+public class homework3 {
+	private static void println(int d, int x £¬string [] args) {
+		   int d=0;
+		   int x=0;
+		   char[] a= {'a','s','v','g'};
+		   for ( int i= 0; i<a,length; i++)  {
+		     if ( a[i] =='a')  {
+		     d++;
+		    }
+		    else if (a[i] = 's'){
+		    x++;
+		    }
+		  }
+		  system out println( d,x  );
+}
+
