6a0b2ca5ac66c9d1b17095b6c17d4524e7202586
sunnanye <89904422+sunnanye@users.noreply.github.com>
2021-09-13 12:46:10 +0800

    Add files via upload

---
+++ homework2.java
@@ -0,0 +1,15 @@
+package com.runoob.text;
+
+public class homework2 {
+	public static void main (String [] args) {
+		   int x;
+		int[] a=new int[x] ;
+		   int i =2;
+		   a[0]=0;
+		   a[1]=1;
+		  while(i<=x) {
+		     a[i]=a[i-1]+a[i-2];
+		     i++ ;
+		 System.out.printf( a[x]);  }
+		 }
+}
