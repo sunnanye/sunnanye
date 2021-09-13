7f036db0d66f712b64938437c8cf2222e8048281
sunnanye <89904422+sunnanye@users.noreply.github.com>
2021-09-13 13:28:03 +0800

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,20 @@
+package com.runoob.text;
+
+public class Circle {
+	private  double  radius;
+	public Circle(double  radius)
+	{  this.radius = radius;
+	}
+	public double setRadius()
+	{  return this.radius;
+	}
+	public void setRadius(double radius) 
+	{  this.radius = radius;
+	}
+	public double getArea() 
+	{  return radius*radius*Math.PI; 
+	}
+	public double getPerimeter() 
+	{ return 2*radius*Math.PI;
+	}
+}
