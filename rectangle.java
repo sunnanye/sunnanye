91599a71fe55009c20a6e7cbfd0dd1565393b95b
sunnanye <89904422+sunnanye@users.noreply.github.com>
2021-09-13 16:15:52 +0800

    Add files via upload

---
+++ rectangle.java
@@ -0,0 +1,22 @@
+package com.runoob.text;
+
+public class rectangle<heigth> {
+	@SuppressWarnings("unused")
+	private heigth width;
+	private Object heigth;
+	public rectangle(heigth width)
+	{this.width = width;
+	}
+	public Object setRadius()
+	{return this.heigth;
+	}
+	public void setRadius(heigth width) 
+	{
+	}
+	public double getArea() 
+	{return height*width; 
+	}
+	public double getPerimeter() 
+	{ return 2(height+width);
+	}
+}
