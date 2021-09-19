58ca417e540249d4f811b726676f222509d78c8a
sunnanye <89904422+sunnanye@users.noreply.github.com>
2021-09-19 13:05:39 +0800

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,13 @@
+package com.runoob.text;
+	public class Student {
+	   public String Name;
+	   public int Age;
+	   private boolean Sex;
+	public boolean isSex() {
+		return Sex;
+	    }
+	public void setSex(boolean Sex) {
+		this.Sex=Sex;
+	    }
+	} 
+}
